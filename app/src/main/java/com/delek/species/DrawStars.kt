78f4c0db2ac64fc3c368d9cdbe6f2b7998eff1ef
manolek1975 <manolek1975@gmail.com>
2024-07-22 09:30:53 +0200

    Draw Scale Background

--- app/src/main/java/com/delek/species/DrawStars.kt
+++ app/src/main/java/com/delek/species/DrawStars.kt
@@ -1,6 +1,7 @@
 package com.delek.species
 
 import android.content.Context
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Paint
@@ -12,28 +13,38 @@ class DrawStars(context: Context): View(context) {
 
     private var posX = 500f
     private var posY = 800f
-    private var drawcolor: Int = ResourcesCompat.getColor(resources, R.color.black, null)
+    private var drawcolor: Int = ResourcesCompat.getColor(resources, R.color.yellow, null)
+
 
     private val paint =  Paint().apply{
         color = drawcolor
-        style = Paint.Style.STROKE
+        style = Paint.Style.FILL
         isAntiAlias = true
+        textSize= 36f
+    }
+    // Función para recoger altura de la barra de acción
+    fun getActionBarHeight(): Int {
+        val ta = context.theme.obtainStyledAttributes(
+            intArrayOf(android.R.attr.actionBarSize)
+        )
+        val actionBarHeight = ta.getDimension(0, 0f).toInt()
+        return actionBarHeight
     }
 
+
+    val bar = getActionBarHeight()
     val dm = resources.displayMetrics
     val fwidth = dm.density * dm.widthPixels
     val fheight = dm.density * dm.heightPixels
-    private var background = BitmapFactory.decodeResource(resources, R.drawable.fondo_sector2)
-    //var resized: Bitmap = Bitmap.createScaledBitmap(background, fwidth.toInt(), fheight.toInt(), true)
-    private var bitmap1 = BitmapFactory.decodeResource(resources, R.drawable.star1)
+    private var bitmap = BitmapFactory.decodeResource(resources, R.drawable.fondo_sector)
+    private val background = Bitmap.createScaledBitmap(
+        bitmap, dm.widthPixels, dm.heightPixels + bar, true)
 
-
-    protected override fun onDraw(canvas: Canvas) {
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawBitmap(background, 0f, 0f, paint)
-        //canvas.drawBitmap(resized, 0f, 0f, paint)
-        canvas.drawBitmap(bitmap1, posX, posY, paint)
+        canvas.drawCircle(posX, posY, 20F, paint)
+        canvas.drawText("Alpha Centauri", posX-20, posY-40, paint)
     }
-
 
 }
